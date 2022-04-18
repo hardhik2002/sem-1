3beedff3919cc1c324021f213b3a8e4abc80290b
Hardhik Reddy <96832540+hardhik2002@users.noreply.github.com>
2022-04-18 10:02:38 +0530

    reema thareeja

---
+++ sum of first 10 numbers.py
@@ -0,0 +1,17 @@
+# this program is also included average of the numbers.
+i=0
+s=0
+while i<=10:
+    s=s+i
+    i=i+1
+avg=float(s)/10
+print("The sum is:",s)
+print("the average is:",avg)
+# similar problem
+m=int(input("Enter the m value:"))
+n=int(input("Enter the n value:"))
+s=0
+while m<=n:
+    s=s+m
+    m=m+1
+print("The sum is:",s)
