4bf5e8e9635e123c32d9efdabaf1163884411e43
Hardhik Reddy <96832540+hardhik2002@users.noreply.github.com>
2022-04-22 10:18:59 +0530

    Update right angle triangle star pattern.py

--- right angle triangle star pattern.py
+++ right angle triangle star pattern.py
@@ -1,2 +1,8 @@
 for i in range(1,(int(input("Enter range:")))+1):
     print("* "*i)
+# altarnetive 
+row=int(input("Enter No.off rows:"))
+for i in range(1,row+1):
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print()
